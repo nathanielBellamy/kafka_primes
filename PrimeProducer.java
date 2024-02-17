8ed565fd7345d40fd3cab186802f78a9fc6ed527
nathanielBellamy <nbschieber@gmail.com>
2024-02-16 20:02:49 -0800

    extend KafkaProduer

--- PrimeProducer.java
+++ PrimeProducer.java
@@ -6,50 +6,58 @@ import java.util.Properties;
 
 public enum ListType { ARRAY_LIST, LINKED_LIST }
 
-public class PrimeProducer {
+public class PrimeProducer extends KafkaProducer {
     static List primes;
 
-    public PrimeProducer(ListType listType) {
-        return switch (listType) {
-            case ARRAY_LIST     -> primes = new ArrayList<Integer>();
-            case LINKED_LIST    -> primes = new LinkedList<Integer>();
-            default             -> throw new IllegalArgumentException("Unrecognized List Type");
-        }
+    public PrimeProducer(ListType listType, Properties props)
+    {
+      switch (listType) {
+          case ARRAY_LIST     -> primes = new ArrayList<Integer>();
+          case LINKED_LIST    -> primes = new LinkedList<Integer>();
+          default             -> throw new IllegalArgumentException("Unrecognized List Type");
+      }
+
+      super(props);
     }
 
-    public void main(String[] args) {
-        String topic = "primes";
-        Properties props = new Properties();
+    public
+    static
+    void
+    main(String[] args)
+    {
+      String topic = "primes";
+      Properties props = new Properties();
 
-        props.put("bootstrap.servers", "localhost:9092");
-        props.put("key.serializer", "org.apache.kafka.common.serialization.IntegerSerializer");
-        props.put("value.serializer", "org.apache.kafka.common.serialization.IntegerSerializer");
+      props.put("bootstrap.servers", "localhost:9092");
+      props.put("key.serializer", "org.apache.kafka.common.serialization.IntegerSerializer");
+      props.put("value.serializer", "org.apache.kafka.common.serialization.IntegerSerializer");
 
-        Producer<Integer, Integer> producer = new KafkaProducer<>(props);
+      Producer<Integer, Integer> producer = new PrimeProducer<>(LINKED_LIST, props);
 
-        primes.add(2);
-        producer.send(new ProducerRecord<>(topic, 2, 2));
+      primes.add(2);
+      producer.send(new ProducerRecord<>(topic, 2, 2));
 
-        int i = 3;
-        while (true)
+      int i = 3;
+      while (true)
+      {
+        boolean isPrime = true;
+        Iterator<Integer> j = primes.iterator();
+        while (j.hasNext())
         {
-          boolean isPrime = true;
-          for (int j = 0; j < primes.size(); j++)
-          {
-            if (i % primes.get(j) == 0) {
-              isPrime = false;
-              break;
-            }
-          }
-
-          if (isPrime)
-          {
-            primes.add(i);
-            i++;
-            producer.send(new ProducerRecord<>(topic, i, i));
+          if (i % j.next() == 0) {
+            isPrime = false;
+            break;
           }
         }
 
-        producer.close();
+        if (isPrime)
+        {
+          primes.add(i);
+          i++;
+          producer.send(new ProducerRecord<>(topic, i, i));
+        }
+      }
+
+      producer.close();
     }
 }
