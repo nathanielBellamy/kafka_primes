08d0081a321968fc3e731ebc411a615ceb8dbb44
nathanielBellamy <nbschieber@gmail.com>
2024-02-17 21:22:43 -0800

    rework consumer

--- src/main/java/dev/nateschieber/kafka_primes/KafkaPrimesApplication.java
+++ src/main/java/dev/nateschieber/kafka_primes/KafkaPrimesApplication.java
@@ -23,10 +23,13 @@ public class KafkaPrimesApplication {
     Properties props = new Properties();
 
     props.put("bootstrap.servers", "localhost:9092");
+    props.put("group.id", "kafka_primes");
     props.put("key.serializer", "org.apache.kafka.common.serialization.IntegerSerializer");
     props.put("value.serializer", "org.apache.kafka.common.serialization.IntegerSerializer");
+    props.put("key.deserializer", "org.apache.kafka.common.serialization.IntegerDeserializer");
+    props.put("value.deserializer", "org.apache.kafka.common.serialization.IntegerDeserializer");
 
-    PrimeConsumer consumer = new PrimeConsumer();
+    PrimeConsumer consumer = new PrimeConsumer(props);
     PrimeProducer producer = new PrimeProducer(ListType.LINKED_LIST, props);
 
     Thread consumerThread = new Thread(consumer);
--- src/main/java/dev/nateschieber/kafka_primes/PrimeConsumer.java
+++ src/main/java/dev/nateschieber/kafka_primes/PrimeConsumer.java
@@ -8,27 +8,27 @@ import java.time.Duration;
 import java.util.Arrays;
 import java.util.Properties;
 
-public class PrimeConsumer extends Thread implements Runnable{
+public class PrimeConsumer extends KafkaConsumer<Integer, Integer> implements Runnable{
+    public
+    PrimeConsumer(Properties props)
+    {
+      super(props);
+      props.put("enable.auto.commit", "true");
+      props.put("auto.commit.interval.ms", "1000");
+    }
+
     public
     void
-    run() {
+    run()
+    {
         String topic = "primes";
-        Properties props = new Properties();
 
-        props.put("bootstrap.servers", "localhost:9092");
-        props.put("group.id", "testGroup");
-        props.put("enable.auto.commit", "true");
-        props.put("auto.commit.interval.ms", "1000");
-        props.put("key.deserializer", "org.apache.kafka.common.serialization.IntegerDeserializer");
-        props.put("value.deserializer", "org.apache.kafka.common.serialization.IntegerDeserializer");
-
-        Consumer<Integer, Integer> consumer = new KafkaConsumer<>(props);
-        consumer.subscribe(Arrays.asList(topic));
+        this.subscribe(Arrays.asList(topic));
 
         while (true)
         {
           // System.out.printf("consume primes");
-          ConsumerRecords<Integer, Integer> records = consumer.poll(Duration.ofMillis(100));
+          ConsumerRecords<Integer, Integer> records = this.poll(Duration.ofMillis(100));
           records.forEach(record -> {
             System.out.printf("offset = %d, key = %d, value = %d%n", record.offset(), record.key(), record.value());
           });
