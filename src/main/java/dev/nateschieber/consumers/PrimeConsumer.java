f0f8e6c0527f98329dd6b1c88ace4ad5020d49cb
nathanielBellamy <nbschieber@gmail.com>
2024-02-18 21:06:37 -0800

    consumer displays each thread

--- src/main/java/dev/nateschieber/consumers/PrimeConsumer.java
+++ src/main/java/dev/nateschieber/consumers/PrimeConsumer.java
@@ -9,6 +9,12 @@ import java.util.Arrays;
 import java.util.Properties;
 
 public class PrimeConsumer extends KafkaConsumer<Integer, Integer> implements Runnable{
+    public int newestArrayPrime  = -1;
+    public int newestLinkedPrime = -1;
+    public int newestVectorPrime = -1;
+
+    public String leader;
+
     public
     PrimeConsumer(Properties props)
     {
@@ -26,7 +32,19 @@ public class PrimeConsumer extends KafkaConsumer<Integer, Integer> implements Ru
           // System.out.printf("consume primes");
           ConsumerRecords<Integer, Integer> records = this.poll(Duration.ofMillis(100));
           records.forEach(record -> {
-            System.out.printf("topic - %s,offset = %d, key = %d, value = %d%n", record.topic(), record.offset(), record.key(), record.value());
+            // System.out.printf("topic - %s,offset = %d, key = %d, value = %d%n", record.topic(), record.offset(), record.key(), record.value());
+            switch (record.topic()) {
+              case "primes_array"   -> this.newestArrayPrime = record.value();
+              case "primes_linked"  -> this.newestLinkedPrime = record.value();
+              case "primes_vector"  -> this.newestVectorPrime = record.value();
+            }
+
+            System.out.printf(
+              "array: %d \nlinked: %d \nvector: %d \n===\n",
+              this.newestArrayPrime,
+              this.newestLinkedPrime,
+              this.newestVectorPrime
+            );
           });
         }
     }
